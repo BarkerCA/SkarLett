67fc48c7cf0743f38794eddbe48ef3390b920fe6
Chris Barker <chris.a.barker@icloud.com>
2015-02-24 21:59:33 -0500

    Frontpage work

--- app/assets/javascripts/skarlett.js
+++ app/assets/javascripts/skarlett.js
@@ -1,8 +1,5 @@
 $(document).ready(function(){
-  //Logo Clicks
   $("#logo").click(function(){
-    window.location = "/";
+    window.location = '/';
   });
-  //Background Image
-  $.backstretch('https://d1nvel87k8rw67.cloudfront.net/www/img/fullstage2_bw.jpg');
 });
