439f59b961653b9c35b0adee22049f3b0ea09dc3
Chris Barker <chris.a.barker@icloud.com>
2015-03-02 15:38:47 -0500

    Disable user signup

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UsersController < ApplicationController
   before_filter :require_authorization, only: [:index, :show, :edit, :update, :destroy]
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :new, :update, :destroy]
 
   def index
     @page = {:title => 'Users Listing', :head_title => 'Users Listing'}
