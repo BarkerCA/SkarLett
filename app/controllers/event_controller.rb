11a715c0b4731f78e83c56f1588f2d6066feafcf
Chris Barker <chris.a.barker@icloud.com>
2015-07-27 13:22:14 -0400

    update to events

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -2,7 +2,7 @@ class EventController < ApplicationController
 
   def index
     @page = {:title => 'Events', :head_title => 'Events'}
-    @events = Event.all.order('date DESC')
+    @events = Event.all.order('date ASC')
   end
 
 end
